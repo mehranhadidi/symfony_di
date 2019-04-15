40dd5a3eb09d46408bd52eb0f276ea41b34ad784
Mehran <mehran.hadidi@gmail.com>
2019-04-15 19:31:35 +0300

    init access manager to check each user with the post voter

--- public/index.php
+++ public/index.php
@@ -6,8 +6,28 @@ error_reporting(E_ALL);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use App\Authorization\AccessManager;
+use App\Authorization\Voter\PostVoter;
+use App\Entity\Post;
+use App\Entity\User;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 // Create an empty container builder
 $containerBuilder = new ContainerBuilder();
 
+$user = new User();
+$user->setUsername('user');
+
+$admin = new User();
+$admin->setUsername('admin');
+$admin->addRole(User::ROLE_ADMIN);
+
+$post = new Post();
+
+$accessManager = new AccessManager([new PostVoter()]);
+
+dump($accessManager->decide(PostVoter::READ, $post, $user));
+dump($accessManager->decide(PostVoter::READ, $post, $admin));
+
+dump($accessManager->decide(PostVoter::WRITE, $post, $user));
+dump($accessManager->decide(PostVoter::WRITE, $post, $admin));
